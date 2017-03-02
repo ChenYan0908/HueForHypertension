e9a242883aab0e45fd73fc5963844d762d054996
Yiyuan Liu <lyy930905@gmail.com>
2017-03-02 14:57:43 +0800

    first commit

---
+++ HueHypertension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HueHypertension
+//
+//  Created by Yiyuan Liu on 2017/3/2.
+//  Copyright © 2017年 Yiyuan Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
